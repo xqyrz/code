89b61e960669fbef3c2c11ee6ffea85634fcac93
xqyr_jv <2513777371@qq.com>
2024-05-03 22:31:53 +0800

    1. 基本字符设备添加请求占用gpio 2.实现按键检测

---
+++ A03_deviceTree＿key/APP/keyapp.h
@@ -0,0 +1,14 @@
+//
+// Created by x on 2/25/24.
+//
+
+#ifndef LEDAPP_BEEPAPP_H
+#define LEDAPP_BEEPAPP_H
+
+#include "chardevapp.h"
+class beepAPP : public charDeviceAPP{
+using charDeviceAPP::charDeviceAPP;
+};
+
+
+#endif //LEDAPP_BEEPAPP_H
